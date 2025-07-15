638fd2d82621baa28bfb06dfcd6ade1efd72352c
Jenius-cn <info@jenius.cn>
2025-07-15 17:32:06 +0800

    add License

--- jenius_mcp.py
+++ jenius_mcp.py
@@ -1,4 +1,19 @@
-# Jenius MCP smart device Server 启动入口
+#    Copyright 2025 jenius-group
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# Jenius MCP smart device Server Entry Point
+
 import os
 import asyncio
 import argparse
